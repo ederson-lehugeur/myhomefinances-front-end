a5ad51cd8571ab2d457e1b560d925738cc12547f
Éderson Lehugeur <eder.lehugeur@gmail.com>
2020-01-17 14:30:54 -0300

    Salvando os dados do usuário logado no localStorage

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,11 +2,13 @@ import { API_CONFIG } from './../config/api.config';
 import { CredenciaisDTO } from './../models/credenciais.dto';
 import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
+import { LocalUser } from '../models/local-user';
+import { StorageService } from './storage.service';
 
 @Injectable()
 export class AuthService {
 
-    constructor(public http: HttpClient) {}
+    constructor(public http: HttpClient, public storage: StorageService) {}
 
     authenticate(creds: CredenciaisDTO) {
         return this.http.post(
@@ -16,5 +18,17 @@ export class AuthService {
                 observe: 'response',
                 responseType: 'text'
             });
+    }
+
+    successfulLogin(authorizationValue: string) {
+        const token = authorizationValue.substring(7);
+        const user: LocalUser = {
+            token: token
+        };
+        this.storage.setlocalUser(user);
+    }
+
+    logout() {
+        this.storage.deleteLocalUser();
     }
 }
---
+++ src/services/storage.service.ts
@@ -0,0 +1,21 @@
+import { STORAGE_KEYS } from './../config/storage-keys.config';
+import { Injectable } from "@angular/core";
+import { LocalUser } from "../models/local-user";
+
+@Injectable()
+export class StorageService {
+
+    getLocalUser() : LocalUser {
+        const usr = localStorage.getItem(STORAGE_KEYS.localUser);
+
+        return usr == null ? null : JSON.parse(usr);
+    }
+
+    setlocalUser(obj: LocalUser) {
+        localStorage.setItem(STORAGE_KEYS.localUser, JSON.stringify(obj));
+    }
+
+    deleteLocalUser() {
+        localStorage.removeItem(STORAGE_KEYS.localUser);
+    }
+}
