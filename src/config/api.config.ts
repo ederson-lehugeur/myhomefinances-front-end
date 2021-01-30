4a7ef2ea75ff1cd3950f6174535c71c286c16081
Éderson Lehugeur <eder.lehugeur@gmail.com>
2021-01-30 23:30:13 +0000

    Atualização do baseUrl em api.config.ts

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
     //baseUrl: "http://localhost:8080"
-    baseUrl: "https://my-home-finances.herokuapp.com"
+    baseUrl: "https://api-myhomefinances.herokuapp.com"
 }
